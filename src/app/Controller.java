c372cfef8cb67bb8bae75b93bfd9d4dd4df2fb70
B.Jones <gehertt@gmail.com>
2019-05-06 11:31:22 +0100

    controller class updated

---
+++ src/app/Controller.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Controller {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Car park free");
+		
+	}
+
+}
---
+++ src/vehiclePark/Grid.java
@@ -0,0 +1,5 @@
+package vehiclePark;
+
+public class Grid {
+
+}
